f38670bec7e1e8056fa189eb678dd46588bf9595
Eero Aaltonen <eero.aaltonen@vaisala.com>
2025-03-19 12:38:17 +0200

    patch picojson to install a CMake config package
    Patch picojson to use CMake for the build and installation and to also
    install a CMake config-file package. The config-file package allows
    downstream CMake builds to:
    
    * check the version of the picojson header library
    * link targets with the `picojson::picojson` interface library, which
        * determines the include directory path automatically
        * propagates the dependency and include directory path forward

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,22 @@
-mingw32-make install includedir="%LIBRARY_INC%\\picojson"
-if %ERRORLEVEL% NEQ 0 exit 1
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+cmake ^
+	"%SRC_DIR%" ^
+	-G "Ninja" ^
+	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+	-DCMAKE_BUILD_TYPE:STRING=Release
+if errorlevel 1 exit 1
+
+:: Build!
+cmake --build . --verbose --parallel "%CPU_COUNT%"
+if errorlevel 1 exit 1
+
+:: Install!
+cmake --build . --verbose --parallel "%CPU_COUNT%" --target install
+if errorlevel 1 exit 1
